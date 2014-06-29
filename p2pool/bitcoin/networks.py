43f0419c81eb263ead422c6f23ae74bbcc88c914
hmsrodrigues <hmsrodrigues@gmail.com>
2014-06-29 20:49:22 +0100

    update networks

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -33,7 +33,9 @@ nets = dict(
         POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('xcoin_hash').getPoWHash(data)),
         BLOCK_PERIOD=60, # s
         SYMBOL='HIRO',
-        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Hirocoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Hirocoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.hirocoin'), 'hirocoin.conf'),
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Hirocoin') if platform.system() == 'Windows' 
+						else os.path.expanduser('~/Library/Application Support/Hirocoin/') if platform.system() == 'Darwin' 
+						else os.path.expanduser('/home/administrator/.hirocoin'), 'hirocoin.conf'),
         BLOCK_EXPLORER_URL_PREFIX='http://explorer.hirocoin.org/block/',
         ADDRESS_EXPLORER_URL_PREFIX='http://explorer.hirocoin.org/address/',
         TX_EXPLORER_URL_PREFIX='http://explorer.hirocoin.org/tx/',
@@ -41,7 +43,30 @@ nets = dict(
         DUMB_SCRYPT_DIFF=1,
         DUST_THRESHOLD=0.001e8,
     ),
-
+	darkcoin=math.Object(
+        P2P_PREFIX='bf0c6bbd'.decode('hex'),
+        P2P_PORT=4334,
+        ADDRESS_VERSION=76,
+        RPC_PORT=3334,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'darkcoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda nBits, height: __import__('darkcoin_subsidy').GetBlockBaseValue(nBits, height),
+        BLOCKHASH_FUNC=lambda data: pack.IntType(256).unpack(__import__('xcoin_hash').getPoWHash(data)),
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('xcoin_hash').getPoWHash(data)),
+        BLOCK_PERIOD=150, # s
+        SYMBOL='DRK',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Darkcoin') if platform.system() == 'Windows' 
+						else os.path.expanduser('~/Library/Application Support/Darkcoin/') if platform.system() == 'Darwin' 
+						else os.path.expanduser('/home/administrator/.darkcoin'), 'darkcoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://explorer.darkcoin.io/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://explorer.darkcoin.io/address/',
+        TX_EXPLORER_URL_PREFIX='http://explorer.darkcoin.io/tx/',
+        SANE_TARGET_RANGE=(2**256//2**32//1000 - 1, 2**256//2**20 - 1),
+        DUMB_SCRYPT_DIFF=1,
+        DUST_THRESHOLD=0.001e8,
+    ),
 
 
 )
--- p2pool/networks.py
+++ p2pool/networks.py
@@ -29,7 +29,24 @@ nets = dict(
         ANNOUNCE_CHANNEL='#p2pool-hic',
         VERSION_CHECK=lambda v: True,
     ),
-
+	darkcoin=math.Object(
+        PARENT=networks.nets['darkcoin'],
+        SHARE_PERIOD=15, # seconds
+        CHAIN_LENGTH=24*60*60//15, # shares
+        REAL_CHAIN_LENGTH=24*60*60//15, # shares
+        TARGET_LOOKBEHIND=200, # shares  //with that the pools share diff is adjusting faster, important if huge hashing power comes to the pool
+        SPREAD=30, # blocks
+        IDENTIFIER='ef05d164bbcd7ed1'.decode('hex'),
+        PREFIX='3966e45ab1ed2db9'.decode('hex'),
+        P2P_PORT=8334,
+        MIN_TARGET=4,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=True,
+        WORKER_PORT=7334,
+        BOOTSTRAP_ADDRS='drk.altmine.net p2pool.plentygadget.com drk2.altmine.net drk3.altmine.net asia01.poolhash.org asia02.poolhash.org q30.qhor.net poulpe.nirnroot.com drk.p2pool.n00bsys0p.co.uk darkcoin.kicks-ass.net darkcoin.fr cryptohasher.net coinminer.net drk.coinquarry.net drk.kopame.com 54.186.8.140 rebootcamp.de'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-drk',
+        VERSION_CHECK=lambda v: v >= 91001,
+    ),
 
 )
 for net_name, net in nets.iteritems():
